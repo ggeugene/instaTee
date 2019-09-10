92889b167a82556468f24ff0107014e00ec7e758
ggeugene <g.eugene.g@outlook.com>
2019-09-10 12:49:33 +0300

    Fix TextSize input for Safari

--- src/components/text-props/TextSize.jsx
+++ src/components/text-props/TextSize.jsx
@@ -1,114 +1,114 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { setTextSize } from '../../actions';
-import { connect } from 'react-redux';
-import MaskedInput from 'react-editmask';
-import { MAX_FONT_SIZE, MIN_FONT_SIZE } from '../../constants';
-import iconMinus from '../../img/icons/icon-minus.png';
-import iconPlus from '../../img/icons/icon-plus.png';
+import React, { useState, useRef, useEffect } from 'react'
+import { setTextSize } from '../../actions'
+import { connect } from 'react-redux'
+import MaskedInput from 'react-editmask'
+import { MAX_FONT_SIZE, MIN_FONT_SIZE } from '../../constants'
+import iconMinus from '../../img/icons/icon-minus.png'
+import iconPlus from '../../img/icons/icon-plus.png'
 
 function TextSize(props) {
-    const [value, setValue] = useState(props.fontSize);
-    const { layerId, setTextSize, getNewCoords, coords, rotateAngle } = props;
-    const input = useRef(null);
+  const [value, setValue] = useState(props.fontSize)
+  const { layerId, setTextSize, getNewCoords, coords, rotateAngle } = props
+  const input = useRef(null)
 
-    useEffect(() => {
-        setValue(props.fontSize);
-    }, [props.fontSize]);
+  useEffect(() => {
+    setValue(props.fontSize)
+  }, [props.fontSize])
 
-    const deselectAll = () => {
-        if (document.selection) {
-            document.selection.empty();
-        } else if (window.getSelection) {
-            window.getSelection().removeAllRanges();
-        }
-    };
+  const deselectAll = () => {
+    if (document.selection) {
+      document.selection.empty()
+    } else if (window.getSelection) {
+      window.getSelection().removeAllRanges()
+    }
+  }
 
-    const applyFontSize = value => {
-        const layer = document.querySelector(`[data-id="${layerId}"]`);
-        layer.style.fontSize = value + 'px';
-    };
+  const applyFontSize = value => {
+    const layer = document.querySelector(`[data-id="${layerId}"]`)
+    layer.style.fontSize = value + 'px'
+  }
 
-    const handleChange = e => {
-        let newCoords = {};
-        let value = Math.floor(parseFloat(e.target.value) * 100) / 100;
-        if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE;
-        if (value < 1) value = 1;
-        if (isNaN(value)) value = '';
-        if (value < MIN_FONT_SIZE) {
-            setValue(value);
-            return;
-        }
-        newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value));
-        setValue(value);
-        setTextSize(layerId, value, newCoords);
-    };
-    const handleSize = e => {
-        e.persist();
-        deselectAll();
-        let newCoords = {};
-        const sign = e.target.dataset.sign;
-        let value = parseFloat(input.current.value);
+  const handleChange = e => {
+    let newCoords = {}
+    let value = Math.floor(parseFloat(e.target.value) * 100) / 100
+    if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE
+    if (value < 1) value = 1
+    if (isNaN(value)) value = ''
+    if (value < MIN_FONT_SIZE) {
+      setValue(value)
+      return
+    }
+    newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value))
+    setValue(value)
+    setTextSize(layerId, value, newCoords)
+  }
+  const handleSize = e => {
+    e.persist()
+    deselectAll()
+    let newCoords = {}
+    const sign = e.target.dataset.sign
+    let value = parseFloat(input.current.value)
 
-        if (sign === '+') {
-            value = ++value;
-            if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE;
-            newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value));
-            setValue(value);
-        } else if (sign === '-') {
-            value = --value;
-            if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE;
-            if (value < 1) value = 1;
-            if (value < MIN_FONT_SIZE) {
-                setValue(value);
-                return;
-            }
-            newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value));
-            setValue(value);
-        }
-        setTextSize(layerId, value, newCoords);
-    };
-    const handleBlur = () => {
-        if (!value) {
-            setValue(props.fontSize);
-        }
-    };
-    return (
-        <div className='text-size__container'>
-            <div>
-                <span className='setting-label'>Text size</span>
-            </div>
-            <div className='flex-row'>
-                <img
-                    src={iconMinus}
-                    data-sign='-'
-                    className='size-control size-decrease'
-                    onClick={handleSize}
-                    alt='-'></img>
-                <MaskedInput
-                    mask='dd?d?(/.d?d?)?'
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    id='text-size'
-                    ref={input}
-                    value={value}
-                />
+    if (sign === '+') {
+      value = ++value
+      if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE
+      newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value))
+      setValue(value)
+    } else if (sign === '-') {
+      value = --value
+      if (value > MAX_FONT_SIZE) value = MAX_FONT_SIZE
+      if (value < 1) value = 1
+      if (value < MIN_FONT_SIZE) {
+        setValue(value)
+        return
+      }
+      newCoords = getNewCoords(layerId, coords, rotateAngle, () => applyFontSize(value))
+      setValue(value)
+    }
+    setTextSize(layerId, value, newCoords)
+  }
+  const handleBlur = () => {
+    if (!value) {
+      setValue(props.fontSize)
+    }
+  }
+  return (
+    <div className='text-size__container'>
+      <div>
+        <span className='setting-label'>Text size</span>
+      </div>
+      <div className='flex-row'>
+        <img
+          src={iconMinus}
+          data-sign='-'
+          className='size-control size-decrease'
+          onClick={handleSize}
+          alt='-'></img>
+        <MaskedInput
+          mask='dd?d?(/.d?d?)?'
+          onChange={handleChange}
+          onBlur={handleBlur}
+          id='text-size'
+          ref={input}
+          value={value}
+        />
 
-                <img
-                    src={iconPlus}
-                    data-sign='+'
-                    className='size-control size-increase'
-                    onClick={handleSize}
-                    alt='+'></img>
-            </div>
-        </div>
-    );
+        <img
+          src={iconPlus}
+          data-sign='+'
+          className='size-control size-increase'
+          onClick={handleSize}
+          alt='+'></img>
+      </div>
+    </div>
+  )
 }
 
 const mapDispatchToProps = dispatch => ({
-    setTextSize: (id, value, coords) => dispatch(setTextSize(id, value, coords)),
-});
+  setTextSize: (id, value, coords) => dispatch(setTextSize(id, value, coords)),
+})
 
 export default connect(
-    null,
-    mapDispatchToProps
-)(TextSize);
+  null,
+  mapDispatchToProps
+)(TextSize)
